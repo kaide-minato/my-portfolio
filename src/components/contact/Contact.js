528648eac3751a162373dd1bd2d904bea2d80fa0
Kaide-Minato <successminato@gmail.com>
2024-06-24 05:13:08 +0900

    portfolio
    this is my portfolio

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Contact.css'
+import Section from '../section/Section'
+import ContactForm from '../contactForm/ContactForm'
+
+const Contact = () => {
+  return (
+    <Section title="Contact">
+      <ContactForm />
+    </Section>
+  )
+}
+
+export default Contact
