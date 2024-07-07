49bf3688fd51dfd62cbabdf63c568d06de5cc7c0
Kaide-Minato <successminato@gmail.com>
2024-07-08 06:08:38 +0900

    portfolio
    this is my portfolio

---
+++ src/components/experience/Experience.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Experience.css'
+import { Fade } from 'react-reveal'
+import Section from '../section/Section'
+import ExperienceCard from '../experienceCard/ExperienceCard'
+import experienceData from '../../data/experience.json'
+
+const Experience = () => {
+  return (
+    <Section title="Experience">
+      <div className="experience-content">
+        <ul className="experience-list">
+          {experienceData.experience.reverse().map((exp) => (
+            <li key={`experience-${exp.company}`}>
+              <Fade bottom duration={1000} distance="20px">
+                <ExperienceCard experience={exp} />
+              </Fade>
+            </li>
+          ))}
+        </ul>
+        <Fade bottom duration={1200} distance="20px">
+        </Fade>
+      </div>
+    </Section>
+  )
+}
+
+export default Experience
